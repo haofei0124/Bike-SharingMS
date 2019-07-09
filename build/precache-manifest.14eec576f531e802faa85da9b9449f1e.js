1853f286ab6e74c00ea8a3008754ffd8158fbf4f
Rosen <udcheng@126.com>
2019-07-08 22:14:30 -0700

    update

---
+++ build/precache-manifest.14eec576f531e802faa85da9b9449f1e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ace947215085da3ad10bf36a8161abd1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fe6d347a2be5b545bcf6",
+    "url": "/static/css/2.2fe65d49.chunk.css"
+  },
+  {
+    "revision": "f2afd95b8698dc344509",
+    "url": "/static/css/main.795991f2.chunk.css"
+  },
+  {
+    "revision": "fe6d347a2be5b545bcf6",
+    "url": "/static/js/2.c8b843d1.chunk.js"
+  },
+  {
+    "revision": "f2afd95b8698dc344509",
+    "url": "/static/js/main.f68bdfef.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
